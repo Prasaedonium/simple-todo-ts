049f321d74115c832d8dd59a9a9d9300ffa403bc
Oscar Pacheco <42507325+Prasaedonium@users.noreply.github.com>
2023-10-29 23:17:25 -0600

    Uploading
    TS src, dist etc

---
+++ src/index.ts
@@ -0,0 +1,77 @@
+// Define the Todo interface
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+// Get DOM elements
+const btn = document.getElementById("btn")!;
+const input = document.getElementById("todoInput")! as HTMLInputElement;
+const form = document.querySelector("#todoform") as HTMLFormElement;
+const list = document.getElementById("todolist")!;
+
+// Initialize the todos array and populate it from local storage
+const todos: Todo[] = readTodos();
+todos.forEach(createTodo);
+
+// Function to read todos from local storage
+function readTodos(): Todo[] {
+  const todosJSON = localStorage.getItem("todos");
+  if (todosJSON === null) return [];
+  return JSON.parse(todosJSON);
+}
+
+// Function to save todos to local storage
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// Event handler for form submission
+function handleSubmit(e: SubmitEvent) {
+  e.preventDefault();
+  const newTodo: Todo = {
+    text: " " + input.value + " ",
+    completed: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  saveTodos();
+  console.log("submitted");
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "✖";
+  deleteButton.classList.add("btn", "btn-danger");
+
+  const newLI = document.createElement("LI");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.classList.add("form-check-input");
+  checkbox.checked = todo.completed;
+  checkbox.addEventListener("change", function () {
+    todo.completed = checkbox.checked;
+    saveTodos();
+  });
+  deleteButton.addEventListener("click", function () {
+    const index = todos.indexOf(todo);
+    if (index > -1) {
+      todos.splice(index, 1);
+      saveTodos();
+      list.removeChild(newLI);
+    }
+  });
+
+  newLI.append(checkbox);
+  newLI.append(todo.text);
+  newLI.append(deleteButton);
+  list.append(newLI);
+}
+
+// Add space element
+
+
+// Event listener for form submission
+form.addEventListener("submit", handleSubmit);
